fa7d55caec54a400df984d65704421bfd125c851
Jack Markert <jsmarkertjs@gmail.com>
2025-11-14 00:48:00 -0500

    app layout

--- app.R
+++ app.R
@@ -1,47 +1,92 @@
 
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+ui <- navbarPage(
+  title = "College Education Analysis",
+  
+  # --- Main Tab 1: Intro ---
+  tabPanel("Intro",
+           h2("Introduction Page"),
+           p("Content for intro page.")
+           
+  ),
+  
+  # --- Main Tab 2: Students and Parents ---
+  tabPanel("Students and Parents",
+           tabsetPanel(
+             
+             # Sub-tab 2a
+             tabPanel("Variable Exploration",
+                      h3("Variable Exploration"),
+                      p("Content here.")
+                      # Add stuff for sub-tab here
+             ),
+             
+             # Sub-tab 2b
+             tabPanel("Best Value Index",
+                      h3("Best Value Index"),
+                      p("Content here.")
+                      # Add stuff for sub-tab here
+             ),
+             
+             # Sub-tab 2c
+             tabPanel("Map",
+                      h3("Map"),
+                      p("Content here.")
+                      # Add map output here
+             )
+           )
+  ),
+  
+  # --- Main Tab 3: Researchers and Administrators ---
+  tabPanel("Researchers and Administrators",
+           tabsetPanel(
+             
+             # Sub-tab 3a
+             tabPanel("Single variable exploration",
+                      h3("Single Variable Exploration"),
+                      p("Content here.")
+                      # Add stuff for sub-tab here
+             ),
+             
+             # Sub-tab 3b
+             tabPanel("Multi-Variable Exploration",
+                      h3("Multi-Variable Exploration"),
+                      p("Content here.")
+                      # Add stuff for sub-tab here
+             ),
+             
+             # Sub-tab 3c
+             tabPanel("Model Testing",
+                      h3("Model Testing"),
+                      p("Content here.")
+                      # Add stuff for sub-tab here
+             )
+           )
+  ),
+  
+  # --- Main Tab 4: Data Table ---
+  tabPanel("Data Table",
+           h2("Data Table Page"),
+           p("This is where the main data table will be displayed."),
+           
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+# --- Define the Server Logic ---
+# The server is where all the calculations and plot/table generation happens.
+# For now, it's empty because we are just building the layout.
+server <- function(input, output, session) {
+  
+  # Server logic for 'Students and Parents' tab...
+  
+  # Server logic for 'Researchers and Administrators' tab...
+  
+  # Server logic for 'Data Table' tab...
+  
 }
 
-# Run the application 
+# --- Run the Application ---
+
 shinyApp(ui = ui, server = server)
